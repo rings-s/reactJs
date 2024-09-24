a9bb2e0a606e9d23fb119d451a25c75da65760bc
rings-s <rings9619@gmail.com>
2024-09-24 19:48:50 -0400

    hero component with using props as parameters

--- react_crash_course/src/App.jsx
+++ react_crash_course/src/App.jsx
@@ -9,7 +9,10 @@ const App = () => {
 
 
 
-      <Hero />
+      <Hero 
+        title='Become a React Dev' 
+        subtitle='Find the React job that fits your skills and needs
+'/>
 
 
  
--- react_crash_course/src/components/Hero.jsx
+++ react_crash_course/src/components/Hero.jsx
@@ -1,5 +1,6 @@
 
-const Hero = () => {
+// we can pass props from App.jsx to Hero.jsx or inside the hero it self as a prop or only or as a parameters in the function
+const Hero = ({ title, subtitle }) => {
   return (
     <div>
 
@@ -8,10 +9,12 @@ const Hero = () => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
           <div className="text-center">
             <h1 className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
-              Become a React Dev
+                {/* Ensure that `title` or props.title is being passed as a prop */}
+                {title}            
             </h1>
             <p className="my-4 text-xl text-white">
-              Find the React job that fits your skills and needs
+                {/* Ensure that `subtitle` or props.subtitle is being passed as a prop */}
+              {subtitle}
             </p>
           </div>
         </div>
