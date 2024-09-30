520641cd189d10a2d30a1b03e5c47954df9659fb
rings-s <rings9619@gmail.com>
2024-09-29 22:56:56 -0400

    adding the addjob form

--- react_crash_course/src/App.jsx
+++ react_crash_course/src/App.jsx
@@ -9,6 +9,7 @@ import MainLayout  from './layouts/MainLayout';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
 import JobPage from './pages/JobPage';
+import AddJobPage from './pages/AddJobPage';
 
 
 // Create a router instance using createBrowserRouter and configure routes
@@ -17,6 +18,7 @@ const router = createBrowserRouter(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} /> 
       <Route path="/jobs" element={<JobsPage />} />
+      <Route path="/add-job" element={<AddJobPage />} />
       <Route path="/jobs/:id" element={<JobPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Route>
